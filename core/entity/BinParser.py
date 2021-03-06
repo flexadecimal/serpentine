c148fbfc6fc11ad65e6c642448acfea21aa77ff5
David Kotaev <flexadecimal>
2021-03-06 17:32:13 -0500

    fix: xdf schema now validates

--- core/entity/BinParser.py
+++ core/entity/BinParser.py
@@ -1,12 +1,16 @@
 #import xml.etree.ElementTree as xml
-from lxml import etree as xml
-import os
+from lxml import (
+  etree as xml,
+  objectify
+)
+import os, sys
 import numpy as np
 import pdb
 
 from pathlib import Path
 core_path = Path(__file__).parent.parent
 schemata_path = os.path.join(core_path, 'schemata')
+xdf_schema_path = 'xdf_schema.xsd'
 
 class BinParser(object):
   '''
@@ -16,21 +20,27 @@ class BinParser(object):
   '''
   def __init__(self, **kwargs):
     adx_tree = xml.parse(kwargs['adx'])
-    # ...get XDF schema
-    xdf_schema = xml.XMLSchema(
-      file = os.path.join(schemata_path, 'xdf_schema.xsd')
-    )
-    xdf_tree = xml.parse(kwargs['xdf'])
-    # ...validate
+    # XDF
+    # ...get schema
     try:
-      xdf_schema.validate(xdf_tree)
-    except XMLSchemaParseError:
-      pdb.set_trace()
-    
+      xdf_schema = xml.XMLSchema(
+        file = os.path.join(schemata_path, xdf_schema_path)
+      )
+    except xml.XMLSchemaParseError as schema_error:
+      print(f"<{type(self).__name__}>: Invalid schema '{xdf_schema_path}'.")
+      print(schema_error)
+      sys.exit()
+    # ...validate
+    xdf_tree = xml.parse(kwargs['xdf'])
+    if not xdf_schema.validate(xdf_tree):
+      print(f"<{type(self).__name__}>: XDF '{kwargs['xdf']}' not validated against schema '{xdf_schema_path}'.")
+      print(xdf_schema.error_log)
+      sys.exit()
     # ...get tables and constants
-    #tables = xdf_tree.xpath('/XDFFORMAT/XDFTABLE')
-    #constants = xdf_tree.xpath('/XDFFORMAT/XDFCONSTANT')
-    
+    tables = xdf_tree.xpath('/XDFFORMAT/XDFTABLE')
+    constants = xdf_tree.xpath('/XDFFORMAT/XDFCONSTANT')
+    # ...parse binary
+    pdb.set_trace()
     
     # xslt functional transformation
     
