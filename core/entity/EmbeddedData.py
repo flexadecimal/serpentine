fixed clamping and endianness. in-progress Axis linking with normalization/interpolation

core/entity/EmbeddedData.py
@@ -1,4 +1,4 @@
-import typing as T
+import typing as t
 import numpy.typing as npt
 # for entities
 from .Base import Base, XdfRefMixin, Array
@@ -10,11 +10,11 @@ from enum import Flag
 class TypeFlags(Flag):
   '''
   Type flags found in binary conversion - endianness, signed, etc.
-  TunerPro uses "LSB first" (big-endian) and "MSB first" (little-endian)
+  TunerPro uses "LSB first" (little-endian) and "MSB first" (big-endian)
   terminology.
   '''
   SIGNED = 1
-  BIG_ENDIAN = 2
+  LITTLE_ENDIAN = 2
   FLOAT = 65536
   COLUMN_MAJOR = 4
 
@@ -23,7 +23,7 @@ class EmbeddedData(Base):
   Used under Math elements, providing details on parsing internal binary data.
   '''
   @property
-  def address(self) -> T.Optional[int]:
+  def address(self) -> t.Optional[int]:
     if 'mmedaddress' in self.attrib:
       return int(self.attrib['mmedaddress'], 16)
     else:
@@ -37,7 +37,7 @@ class EmbeddedData(Base):
     return int(self.attrib['mmedelementsizebits']) // 8
 
   @property
-  def shape(self) -> T.Union[T.Tuple[int], T.Tuple[int, int]]:
+  def shape(self) -> t.Union[t.Tuple[int], t.Tuple[int, int]]:
     '''
     Shape tuple following Numpy convention - 2D tables like that of `Table.ZAxis` have shape like `(16, 16)` (or occasionally `(16, )` for a 1D table). Constants have shape `(1, )` - a single number that will be broadcoast to an array length 1.
     '''
@@ -66,13 +66,13 @@ class EmbeddedData(Base):
       type = 'i'
     else:
       type = 'u'
-    endianness = '>' if TypeFlags.BIG_ENDIAN in self.type_flags else '<'
+    endianness = '<' if TypeFlags.LITTLE_ENDIAN in self.type_flags else '>'
     type_str = f'{endianness}{type}{self.length}'
     return np.dtype(type_str)
 
   # table only
   @property
-  def strides(self) -> T.Optional[T.Union[T.Tuple[int], T.Tuple[int, int]]]:
+  def strides(self) -> t.Optional[t.Union[t.Tuple[int], t.Tuple[int, int]]]:
     '''
     Array stride in memory.
     '''
@@ -85,10 +85,10 @@ class EmbeddedData(Base):
     else:
       raise ValueError
 
-class EmbeddedMathMixin(XdfRefMixin):
+class Embedded(XdfRefMixin):
   '''
   Mixin for objects exposing a value using both `<MATH>` and `<EMBEDDEDDATA>`.
-  `Constant` and `Table.Axis` do this. `Table` has a value itself, but its value is constructed from "real" `EmbeddedMathMixin` memory maps.
+  `Constant` and `Table.Axis` do this. `Table` has a value itself, but its value is constructed from "real" `Embedded` memory maps.
   '''
   EmbeddedData: EmbeddedData = Base.xpath_synonym('./EMBEDDEDDATA')
 

core/entity/Parameter.py
@@ -3,14 +3,24 @@ from abc import ABC, abstractmethod
 import numpy as np
 import typing as T
 from .Category import Categorized
+from enum import Flag
+
+class ParameterFlags(Flag):
+  '''
+  Controls optionally-clamped output.
+  '''
+  MIN_CLAMPED = 16
+  MAX_CLAMPED = 32
 
 class Parameter(Categorized, Base, XdfRefMixin, ABC, metaclass=XmlAbstractBaseMeta):
   id: str = Base.xpath_synonym('./@uniqueid')
   title: str = Base.xpath_synonym('./title/text()')
   description: T.Optional[str] = Base.xpath_synonym('./description/text()')
-  # TODO: codify using XDF-local enum?
-  # see https://docs.python.org/3/library/enum.html#functional-api
-  Category = Base.xpath_synonym('./CATEGORYMEM')
+  # when None, always visible. TODO: actually implement
+  @property
+  def visibiity(self) -> T.Optional[int]:
+    out = self.xpath('./@vislevel')
+    return int(out[0]) if out else None
 
   @property
   @abstractmethod
@@ -32,27 +42,37 @@ class Parameter(Categorized, Base, XdfRefMixin, ABC, metaclass=XmlAbstractBaseMe
   #  ))
   #  return f"<{self.__class__.__qualname__} id='{self.id}'>{str(filtered_vars)}"
 
-class ClampedMixin(Base):
+class Clamped(Base):
   '''
   For `Parameter`s like `Table` and `Constant` that have optional min/max clamped outputs.
 
   Constant uses xml <rangehlow>, <rangehigh> which is very strange. They also have defaults, per class.
   - Constant, Table.Axis: 0 - 255
   - Function.Axis: 0 - 1000
+
+  TODO: implement defaults
   '''
+  @property
+  def flags(self) -> ParameterFlags:
+    # if this was in Parameter class...
+    #out = self.xpath('./@flags')
+    # ... but actually we are in child of Parameter
+    out = self.getparent().attrib['flags']
+    return ParameterFlags(int(out, 16))
+    
   @property
   def min(self) -> T.Optional[float]:
     out = self.xpath('./min/text()')
-    return float(out[0]) if out else None
+    return float(out[0]) if out and ParameterFlags.MIN_CLAMPED in self.flags else None
   
   @property
   def max(self) -> T.Optional[float]:
     out = self.xpath('./max/text()')
-    return float(out[0]) if out else None
+    return float(out[0]) if out and ParameterFlags.MAX_CLAMPED in self.flags else None
 
   def clamped(self, x: ArrayLike) -> ArrayLike:
     # either can be None, not both
     if not (self.min is None and self.max is None):
-        return np.clip(x, self.min, self.max) # type: ignore
+      return np.clip(x, self.min, self.max) # type: ignore
     else:
       return x

core/entity/Table.py
@@ -1,10 +1,10 @@
-import typing as T
+import typing as t
 from abc import ABC, abstractmethod
 from .Base import Base, Quantity, ArrayLike, XmlAbstractBaseMeta, Array
 from .Math import Math
 from .EmbeddedData import EmbeddedData
-from .Axis import Axis
-from .Parameter import Parameter, ClampedMixin
+from .Axis import Axis, EmbeddedAxis, XYEmbeddedAxis, XYLabelAxis, XYLinkAxis
+from .Parameter import Parameter, Clamped
 import numpy as np
 import numpy.typing as npt
 import functools
@@ -88,18 +88,18 @@ class CellMath(MaskedMath):
     out[self.row][self.column] = 1
     return Mask(np.logical_not(out))
 
-class ZAxis(Axis, ClampedMixin):
+class ZAxis(EmbeddedAxis, Clamped):
   '''
   Special-case axis, generally referred to interchangeably with as a "Table", although the Table really contains the Axes and their related information.
   '''
-  Math: T.List[_math] = Base.xpath_synonym('./MATH', many=True) # type: ignore
+  Math: t.List[_math] = Base.xpath_synonym('./MATH', many=True) # type: ignore
   global_Math: GlobalMath = Base.xpath_synonym('./MATH[not(@row) and not(@col)]')
-  column_Math: T.List[ColumnMath] = Base.xpath_synonym('./MATH[@col and not(@row)]', many=True)
-  row_Math: T.List[RowMath] = Base.xpath_synonym('./MATH[@row and not(@col)]', many=True)
-  cell_Math: T.List[CellMath] = Base.xpath_synonym('./MATH[@row and @col]', many=True)
+  column_Math: t.List[ColumnMath] = Base.xpath_synonym('./MATH[@col and not(@row)]', many=True)
+  row_Math: t.List[RowMath] = Base.xpath_synonym('./MATH[@row and not(@col)]', many=True)
+  cell_Math: t.List[CellMath] = Base.xpath_synonym('./MATH[@row and @col]', many=True)
 
   @staticmethod
-  def _combine_masks(*masks: Mask, initial: T.Optional[Mask] = None) -> Mask:
+  def _combine_masks(*masks: Mask, initial: t.Optional[Mask] = None) -> Mask:
     # un-invert combined mask...
     reducer = lambda a, b: np.logical_not(
       # ... after inverting masks so that OR works
@@ -113,8 +113,8 @@ class ZAxis(Axis, ClampedMixin):
   def _mask_reduction(
     self,
     accumulator: np.ma.MaskedArray,
-    type_math: T.Tuple[T.Type[MaskedMath], MaskedMath],
-    group_masks: T.Dict[T.Type[MaskedMath], Mask]
+    type_math: t.Tuple[t.Type[MaskedMath], MaskedMath],
+    group_masks: t.Dict[t.Type[MaskedMath], Mask]
   ):
     '''
     Used internally in `Table.ZAxis`'s binary conversion, where each `math: MaskedMath` takes a masked view of an original array and converts parts incrementally.
@@ -166,7 +166,7 @@ class ZAxis(Axis, ClampedMixin):
 
     # TABLE MATH CONVERSION
     # ...in order of lowest to highest precedence
-    math_groups: T.Dict[T.Type[MaskedMath], T.List[MaskedMath]] = {
+    math_groups: t.Dict[t.Type[MaskedMath], t.List[MaskedMath]] = {
       GlobalMath: [self.global_Math],
       ColumnMath: self.column_Math,
       RowMath: self.row_Math,
@@ -178,7 +178,7 @@ class ZAxis(Axis, ClampedMixin):
       if maths
     }
     # ...masks must be subtracted - combine masks amongst groups
-    combined_masks: T.Dict[T.Type[MaskedMath], npt.NDArray] = {
+    combined_masks: t.Dict[t.Type[MaskedMath], npt.NDArray] = {
       klass: self._combine_masks(
         *map(lambda math: math.mask, maths)
       )
@@ -205,27 +205,25 @@ class ZAxis(Axis, ClampedMixin):
     # optionally clamp
     return self.clamped(out)
 
+XYAxis = t.Union[XYEmbeddedAxis, XYLabelAxis, XYLinkAxis]
 class Table(Parameter):
   '''
   Table, a.k.a array/list of values. Usually this is a 2D table like a fuel or ignition map, or occasionally, a 1D list like an axis, e.g. Major RPM.
   '''
-  @property
-  def Axes(self) -> dict[str, Axis]:
-    '''
-    3D surface with X, Y, and Z axes.
-    '''
-    return {axis.attrib['id']: axis for axis in self.xpath('./XDFAXIS')}
+  x: XYAxis = Base.xpath_synonym("./XDFAXIS[@id='x']")
+  y: XYAxis = Base.xpath_synonym("./XDFAXIS[@id='y']")
+  z: XYAxis = Base.xpath_synonym("./XDFAXIS[@id='z']")
 
   @property
   def value(self):
-    return self.Axes['z'].value
+    return self.z.value
 
   def __str__(self):
     sep = ' '
     width = 6
     fmt = f"{{0:>{width}.1f}}"
     x_axis = sep.join(map(
-      lambda x: fmt.format(x), self.Axes['x'].value
+      lambda x: fmt.format(x), self.x.value
     ))
     x_preceding = ' ' * (width + 2 + len(sep))
     line = '-' * len(x_axis) 
@@ -233,12 +231,12 @@ class Table(Parameter):
     zs_with_y = '\n'.join(
       sep.join([
         # preceding y val
-        fmt.format(self.Axes['y'].value[index]),
+        fmt.format(self.y.value[index]),
         # divider
        '|',
         # z vals
         *map(lambda z: fmt.format(z), row)
-      ]) for index, row in enumerate(self.Axes['z'].value)
+      ]) for index, row in enumerate(self.z.value)
     )
     return f"{x_preceding}{x_axis}\n{x_preceding}{line}\n{zs_with_y}"
     